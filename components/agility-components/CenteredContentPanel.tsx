2f02fb211615f21fc38bfc13b421db0ec3b42a2c
Joel Varty <joel@agilitycms.com>
2024-07-15 12:55:59 -0400

    section

--- components/agility-components/CenteredContentPanel.tsx
+++ components/agility-components/CenteredContentPanel.tsx
@@ -26,7 +26,7 @@ export const CenteredContentPanel = async ({ module, languageCode }: UnloadedMod
 	return (
 		<Container id={`${contentID}`} data-agility-component={contentID}>
 			<div className="max-w-5xl mx-auto text-center ">
-				{section && <h5 className="text-lg font-medium mb-3">{section}</h5>}
+				{section && <h5 className="text-lg font-medium mb-5 uppercase text-gray-500">{section}</h5>}
 				{title && <h1 className="text-5xl text-balance font-medium">{title}</h1>}
 				{description && (
 					<div className="mt-8 font-medium text-2xl" dangerouslySetInnerHTML={renderHTML(description)} />
