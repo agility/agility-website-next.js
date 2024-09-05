aa68648606d7474ac5efcb369ca3e5b51052060c
Joel Varty <joel@agilitycms.com>
2024-09-05 17:14:17 -0400

    scheduler fix

--- components/agility-components/ScheduleADemo/ScheduleADemo.client.tsx
+++ components/agility-components/ScheduleADemo/ScheduleADemo.client.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Script from "next/script"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 interface Props {
 	schedulerIFrameURL: string
@@ -10,15 +10,25 @@ export const ScheduleADemoClient = ({ schedulerIFrameURL }: Props) => {
 	const refIframe = useRef<HTMLIFrameElement>(null)
 	const [iframeUrl, setIframeUrl] = useState("")
 
+	useEffect(() => {
+		if (typeof window === "undefined") return
+
+		//add the script to the page dynamically
+		const script = document.createElement("script")
+		script.src = "https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"
+		script.async = true
+		document.body.appendChild(script)
+
+		return () => {
+			if (typeof window === "undefined") return
+			//clean up the script when the component is unmounted
+			document.body.removeChild(script)
+		}
+	}, [])
+
 	return (
 		<>
-			<Script
-				src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"
-				onLoad={() => {
-					setIframeUrl(schedulerIFrameURL)
-				}}
-			/>
-			{iframeUrl && <div className="meetings-iframe-container h-[800px] w-full" data-src={iframeUrl}></div>}
+			<div className="meetings-iframe-container h-[800px] w-full" data-src={schedulerIFrameURL}></div>
 		</>
 	)
 }
