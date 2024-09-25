1627af02ddad84a1ad2a4e0bee1746cb48a67d68
Joel Varty <joel@agilitycms.com>
2024-09-25 14:44:38 -0400

    firefox fix

--- components/agility-components/VerticalContentPanel/VerticalContentPanel.client.tsx
+++ components/agility-components/VerticalContentPanel/VerticalContentPanel.client.tsx
@@ -30,49 +30,51 @@ export const VerticalContentPanelClient = ({ contentID, panels, textSide }: Prop
 	const [open, setOpen] = useState(true)
 
 	return (
-		<div className={clsx("lg:flex gap-2 items-center", textSide === "right" ? "lg:flex-row-reverse" : "")}>
-			<div className="lg:w-1/2 lg:flex lg:flex-col">
+		<div className={clsx("items-center gap-2 lg:flex", textSide === "right" ? "lg:flex-row-reverse" : "")}>
+			<div className="lg:flex lg:w-1/2 lg:flex-col">
 				{panels.map((panel, index) => (
 					<div
 						key={panel.title}
-						className="flex flex-col-reverse md:flex-row gap-2 lg:block md:odd:flex-row md:even:flex-row-reverse "
+						className="flex flex-col-reverse gap-2 md:flex-row md:odd:flex-row md:even:flex-row-reverse lg:block"
 					>
 						<label
 							className={clsx(
-								"block relative lg:cursor-pointer group lg:hover:bg-background p-8 pt-4 transition-colors text-left",
-								"lg:has-[:checked]:bg-background ",
+								"group relative block p-8 pt-4 text-left transition-colors lg:cursor-pointer lg:hover:bg-background",
+								"lg:has-[:checked]:bg-background",
 								"m-auto md:w-1/2 lg:w-auto"
 							)}
 							onMouseOver={() => activatePanel(index)}
 						>
-							<input
-								type="radio"
-								name={idStr}
-								id={`${idStr}-${index}`}
-								className="peer h-0 w-0 overflow-hidden"
-								value={index}
-								checked={activePanel === index}
-								onChange={() => {
-									setOpen(false)
+							<div className="invisible">
+								<input
+									type="radio"
+									name={idStr}
+									id={`${idStr}-${index}`}
+									className="-top-100 -left-100 peer absolute h-0 w-0 overflow-hidden"
+									value={index}
+									checked={activePanel === index}
+									onChange={() => {
+										setOpen(false)
 
-									setTimeout(() => {
-										setActivePanel(index)
-										setOpen(true)
-									}, 300)
-								}}
-							/>
+										setTimeout(() => {
+											setActivePanel(index)
+											setOpen(true)
+										}, 300)
+									}}
+								/>
+							</div>
 							<h4
 								className={clsx(
-									"text-lg lg:group-hover:text-highlight-light font-medium transition-colors lg:peer-checked:text-highlight-light"
+									"text-lg font-medium transition-colors lg:group-hover:text-highlight-light lg:peer-checked:text-highlight-light"
 								)}
 							>
 								{panel.title}
 							</h4>
 
-							<div className="w-0.5 h-0 lg:group-hover:h-full lg:peer-checked:h-full transition-all duration-300 bg-highlight-light absolute top-0 left-0"></div>
-							<div className="mt-2 prose" dangerouslySetInnerHTML={renderHTML(panel.description)}></div>
+							<div className="absolute left-0 top-0 h-0 w-0.5 bg-highlight-light transition-all duration-300 lg:group-hover:h-full lg:peer-checked:h-full"></div>
+							<div className="prose mt-2" dangerouslySetInnerHTML={renderHTML(panel.description)}></div>
 						</label>
-						<div className="lg:hidden md:w-1/2 ">
+						<div className="md:w-1/2 lg:hidden">
 							{panels[activePanel].graphic.url.endsWith(".svg") ? (
 								<img src={panels[activePanel].graphic.url} alt="" className="w-full" />
 							) : (
@@ -82,15 +84,15 @@ export const VerticalContentPanelClient = ({ contentID, panels, textSide }: Prop
 					</div>
 				))}
 			</div>
-			<div className="w-1/2 hidden lg:block">
+			<div className="hidden w-1/2 overflow-clip lg:block">
 				<Transition show={open} as={"div"} className={"relative"}>
 					<TransitionChild>
 						<div
 							className={clsx(
-								"absolute top-0 left-0",
-								"transition-all duration-300 data-[closed]:duration-0 ease-in",
-								"data-[closed]:opacity-0 data-[closed]:h-0 h-auto",
-								"data-[closed]:mt-10 mt-0"
+								"absolute left-0 top-0",
+								"transition-all duration-300 ease-in data-[closed]:duration-0",
+								"h-auto data-[closed]:h-0 data-[closed]:opacity-0",
+								"mt-0 data-[closed]:mt-10"
 							)}
 						>
 							<img src="/assets/layout/layer-content-image.png" alt="" />
@@ -100,17 +102,21 @@ export const VerticalContentPanelClient = ({ contentID, panels, textSide }: Prop
 					<TransitionChild>
 						<div
 							className={clsx(
-								"transition-all duration-300 data-[closed]:duration-0 ease-in",
-								"data-[closed]:opacity-0 data-[closed]:h-0 h-auto",
-								"data-[closed]:mt-10 mt-0"
+								"transition-all duration-300 ease-in data-[closed]:duration-0",
+								"h-auto data-[closed]:h-0 data-[closed]:opacity-0",
+								"mt-0 h-full data-[closed]:mt-10"
 							)}
 						>
 							{panels[activePanel].graphic.url.endsWith(".svg") ? (
-								<img src={panels[activePanel].graphic.url} alt="" className="w-full relative" />
+								<img
+									src={panels[activePanel].graphic.url}
+									alt=""
+									className="relative w-full bg-white"
+								/>
 							) : (
 								<AgilityPic
 									image={panels[activePanel].graphic}
-									className="w-full relative z-10"
+									className="relative z-10 h-[500px] w-full bg-white object-contain"
 									fallbackWidth={600}
 								/>
 							)}
