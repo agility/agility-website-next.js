fbca8662a3ca185fbc03ea0dfdf7fa77c94287c3
Joel Varty <joel@agilitycms.com>
2025-02-04 10:29:17 -0500

    perf tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ import { Mulish } from 'next/font/google'
 // If loading a variable font, you don't need to specify the font weight
 const mulish = Mulish({
 	subsets: ['latin'],
-	display: 'auto',
+	display: 'swap', //using swap to avoid render blocking
 	variable: '--font-mulish',
 })
 
