adca77975c223f3b18a24e64dfc67dfe0893f53d
Joel Varty <joel@agilitycms.com>
2025-12-03 10:51:03 -0500

    refactor: Update GartnerPeerInsightsBar layout and enhance CSS utility classes
    - Adjusted the layout of the GartnerPeerInsightsBar component for improved structure and spacing.
    - Updated the LinkButton placement within the component for better visual hierarchy.
    - Added new utility classes in output.css for margin, padding, and positioning adjustments to enhance responsiveness and layout control.

--- components/agility-components/GartnerPeerInsightsBar.tsx
+++ components/agility-components/GartnerPeerInsightsBar.tsx
@@ -19,29 +19,31 @@ export const GartnerPeerInsightsBar = async ({ module, languageCode }: UnloadedM
 	})
 
 	return (
-		<div className="relative items-center bg-gradient-to-r from-highlight to-highlight-dark p-8">
-			<div className="absolute left-0 h-full overflow-clip">
-				<img src="https://static.agilitycms.com/layout/static/bg-top.svg" alt="" />
-			</div>
-			<div className="absolute right-0 h-full overflow-clip">
-				<img src="https://static.agilitycms.com/layout/static/bg-top.svg" alt="" />
-			</div>
-			<div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-3 lg:flex-row">
-				<div className="flex flex-1 flex-col items-center gap-5 md:flex-row">
-					<div className="h-12 border-r border-r-white/20 pr-5">
-						<AgilityPic image={fields.gartnerLogo} className="h-full object-contain" fallbackWidth={400} />
-					</div>
-					<div className="h-6">
-						<AgilityPic image={fields.starsGraphic} className="h-full object-contain" fallbackWidth={300} />
+		<div className="pt-14">
+			<div className="relative items-center bg-gradient-to-r from-highlight to-highlight-dark p-8">
+				<div className="absolute left-0 h-full overflow-clip">
+					<img src="https://static.agilitycms.com/layout/static/bg-top.svg" alt="" />
+				</div>
+				<div className="absolute right-0 h-full overflow-clip">
+					<img src="https://static.agilitycms.com/layout/static/bg-top.svg" alt="" />
+				</div>
+				<div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-3 lg:flex-row">
+					<div className="flex flex-1 flex-col items-center gap-5 md:flex-row">
+						<div className="h-12 border-r border-r-white/20 pr-5">
+							<AgilityPic image={fields.gartnerLogo} className="h-full object-contain" fallbackWidth={400} />
+						</div>
+						<div className="h-6">
+							<AgilityPic image={fields.starsGraphic} className="h-full object-contain" fallbackWidth={300} />
+						</div>
+						<div>
+							<h3 className="text-lg font-medium text-white">{fields.title}</h3>
+						</div>
 					</div>
 					<div>
-						<h3 className="text-lg font-medium text-white">{fields.title}</h3>
+						<LinkButton type="primary" size="lg" href={fields.cTAButton.href} target={fields.cTAButton.target}>
+							{fields.cTAButton.text}
+						</LinkButton>
 					</div>
-				</div>
-				<div>
-					<LinkButton type="primary" size="lg" href={fields.cTAButton.href} target={fields.cTAButton.target}>
-						{fields.cTAButton.text}
-					</LinkButton>
 				</div>
 			</div>
 		</div>
