2c418861dc8d288a0adb23555491a29f1b903f94
Joel Varty <joel@agilitycms.com>
2026-01-23 16:13:43 -0500

    feat: Add Cache-Control header to LLMSText GET response

--- app/llms.txt/route.ts
+++ app/llms.txt/route.ts
@@ -16,7 +16,8 @@ export async function GET() {
 
 	return new NextResponse(textBlob, {
 		headers: {
-			"Content-Type": "text/plain; charset=utf-8"
+			"Content-Type": "text/plain; charset=utf-8",
+			"Cache-Control": "public, s-maxage=60, stale-while-revalidate=604800"
 		}
 	})
 }
